119a1959cd8b6250969bb8f36f6f4b35cc9eef8c
Troy Flagg <tf3193@gmail.com>
2020-04-29 19:55:40 -0400

    Added a constants file and replaced all hard coded names with the constant value.

--- consumer.py
+++ consumer.py
@@ -1,18 +1,18 @@
 from kafka import KafkaConsumer
 import ast
+import constants
 import config
 
-
 class Consumer:
-
-    consumer = KafkaConsumer('resource-monitor-metrics', client_id = "frontend", consumer_timeout_ms=4.95)
+    consumer = KafkaConsumer('resource-monitor-metrics', client_id="frontend")
+    # This is initialized just for sanity sake and for a bit of clarity for modifying in the future.
     metrics = {
-        "cpu": [],
-        "memory": [],
-        "faults": [],
-        "cpu_core": {},
-        "processes": {},
-        "network": [{}]
+        constants.CPU: [],
+        constants.MEMORY: [],
+        constants.PAGE_FAULTS: [],
+        constants.CPU_CORE: {},
+        constants.PROCESSES: {},
+        constants.NETWORK: [{}]
     }
 
     def poll_consumer(self):
@@ -20,32 +20,39 @@ class Consumer:
         Call poll on the consumer and then store the metrics gathered into the metrics dictionary
         :return:
         """
+
         poll_results = self.consumer.poll(timeout_ms=5000)
         # I need the list of keys because poll results unlike messages inside of consumer, is a dictionary not a list.
         keys = list(poll_results)
+        # This handles an edge case to fail gracefully.
         if len(keys) == 0:
             return
+
         for message in poll_results[keys[0]]:
             # Convert the message from bytes to utf
             dict_str = message.value.decode("UTF-8")
-            mydata = ast.literal_eval(dict_str)
-            metric_type = mydata['metric_type']
+            metric_dict = ast.literal_eval(dict_str)
+            metric_type = metric_dict['metric_type']
 
-            if metric_type == 'memory':
-                self.metrics['memory'].append(mydata['utilization'])
-                self.metrics['faults'].append(mydata['page_faults'])
-            elif metric_type == 'cpu':
-                self.metrics['cpu'].append(mydata['utilization'])
-            elif metric_type == 'processes':
-                # Check the configured memory value in config.py, as long as we are using more we will keep track of
-                # this process
-                if config.MIN_MEMORY <= mydata['memory']:
-                    self.metrics['processes'][mydata['pid']] = {'memory': mydata['memory'], 'state': mydata['state'],
-                                            'name': mydata['name']}
-            elif metric_type == 'cpu_core':
-                self.metrics['cpu_core'][mydata['core_id']] = mydata['core_utilization']
-            elif metric_type == 'network':
-                self.metrics['network'].append({"send": mydata['send'], 'receive': mydata['receive']})
+            if metric_type == constants.MEMORY:
+                self.metrics[constants.MEMORY].append(metric_dict[constants.UTILIZATION])
+                self.metrics[constants.PAGE_FAULTS].append(metric_dict[constants.PAGE_FAULTS])
+            elif metric_type == constants.CPU:
+                self.metrics[constants.CPU].append(metric_dict[constants.UTILIZATION])
+            elif metric_type == constants.PROCESSES:
+                # Check the constantsured memory value in constants.py, as long as we are using more we will keep
+                # track of this process
+                if config.MIN_MEMORY <= metric_dict[constants.MEMORY]:
+                    self.metrics[constants.PROCESSES][metric_dict[constants.PID]] = {
+                        constants.MEMORY: metric_dict[constants.MEMORY],
+                        constants.STATE: metric_dict[constants.STATE],
+                        constants.NAME: metric_dict[constants.NAME]}
+            elif metric_type == constants.CPU_CORE:
+                self.metrics[constants.CPU_CORE][metric_dict[constants.CORE_ID]] = \
+                    metric_dict[constants.CORE_UTILIZATION]
+            elif metric_type == constants.NETWORK:
+                self.metrics[constants.NETWORK].append({constants.SEND: metric_dict[constants.SEND],
+                                                        constants.RECEIVE: metric_dict[constants.RECEIVE]})
 
     def get_memory_metrics(self):
         """
@@ -53,7 +60,7 @@ class Consumer:
         :return: float
         """
         self.poll_consumer()
-        return self.metrics['memory'][-1]
+        return self.metrics[constants.MEMORY][-1]
 
     def get_network_metrics(self):
         """
@@ -61,15 +68,7 @@ class Consumer:
         :return: dict
         """
         self.poll_consumer()
-        return self.metrics['network'][-1]
-
-    def get_system_metrics(self):
-
-        for message in self.consumer:
-            dict_str = message.value.decode("UTF-8")
-            mydata = ast.literal_eval(dict_str)
-            if mydata['metric_type'] == 'system_metrics':
-                return mydata
+        return self.metrics[constants.NETWORK][-1]
 
     def get_process_metrics(self):
         """
@@ -77,12 +76,10 @@ class Consumer:
         :return: dict
         """
         self.poll_consumer()
-        #There is an edge case where the first call only gathers memory/cpu etc so until we find any process metrics we loop
-        while len(list(self.metrics['processes'])) <= 1:
+        # There is an edge case where the first call only gathers memory/cpu etc so until we find any process metrics we loop
+        while len(list(self.metrics[constants.PROCESSES])) < 1:
             self.poll_consumer()
-        return self.metrics['processes']
-
-
+        return self.metrics[constants.PROCESSES]
 
     def get_cpu_core_metrics(self):
         """
@@ -90,7 +87,7 @@ class Consumer:
         :return: dict
         """
         self.poll_consumer()
-        return self.metrics['cpu_core']
+        return self.metrics[constants.CPU_CORE]
 
     def get_cpu_metrics(self):
         """
@@ -98,5 +95,4 @@ class Consumer:
         :return: float
         """
         self.poll_consumer()
-        return self.metrics['cpu'][-1]
-
+        return self.metrics[constants.CPU][-1]
